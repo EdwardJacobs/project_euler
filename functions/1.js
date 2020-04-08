223796ee225aa223c9af183c9e34225e2a2b91d3
Edward Jacobs <eddielawrence23@gmail.com>
2020-04-08 14:07:54 -0500

    Solve 1

---
+++ functions/1.js
@@ -0,0 +1,11 @@
+function sumMultiples(number) {
+  let sum = 0;
+  for (let i=0; i<number; i++) {
+    if (i%5 === 0 || i%3 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+module.exports = sumMultiples;
---
+++ tests/1.test.js
@@ -0,0 +1,6 @@
+const sumMultiples = require('../functions/1');
+
+test('sumMultiples function exists', () => {
+  expect(sumMultiples).toBeDefined();
+});
+
