bf4aad753e0c21f8be352073870aa5c2ef961a80
Edward Jacobs <eddielawrence23@gmail.com>
2020-04-21 23:47:09 -0500

    Partial solution 3

--- functions/3.js
+++ functions/3.js
@@ -1,7 +1,13 @@
 // find all primes less than number
 // return highest prime less than number
 function largestPrime(number) {
+  arr = [];
 
+  for (let i=2; i<number; i++) {
+    if (number%i != 0 && number != i) {
+      arr.push(arr[i]);
+    }
+  }
 }
 
 module.exports = largestPrime;
