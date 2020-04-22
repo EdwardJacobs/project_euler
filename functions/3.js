5224f4d051d420003cff5f32736f3e5bd6bca70b
Edward Jacobs <eddielawrence23@gmail.com>
2020-04-21 23:35:44 -0500

    Begin 3

---
+++ functions/3.js
@@ -0,0 +1,7 @@
+// find all primes less than number
+// return highest prime less than number
+function largestPrime(number) {
+
+}
+
+module.exports = largestPrime;
---
+++ tests/3.test.js
@@ -0,0 +1,5 @@
+const largestPrime = require('../functions/3');
+
+test('largestPrime function exists', () => {
+  expect(largestPrime).toBeDefined();
+})
